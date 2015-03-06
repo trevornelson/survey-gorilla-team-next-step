45652cb72ea7c045d37d5c1a7addefde384afcde
Notika314 <notika314@gmail.com>
2015-03-06 18:25:19 -0500

    add forms

--- app/controllers/authentication.rb
+++ app/controllers/authentication.rb
@@ -1,7 +1,29 @@
 get "/authentication/login" do
-  erb :'authentication/login'
+  erb :'auth/login'
 end
 
 get "/authentication/signup" do
-  erb :"authentication/signup"
+  erb :"auth/signup"
+end
+
+post "/authentication/register_user" do
+  if params[:password] == params[:password_confirm]
+    user = User.new(params)
+    user.save
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    redirect '/?error=true'
+  end
+
+end
+
+post "authentication/signin" do
+  user = User.find_by(email: params[:email])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect "/users/#{user.id}"
+  else
+    redirect '/?error=true'
+  end
 end
