892b12598af7c841c8076f5a381b8a290b0c8dd3
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-08 17:09:19 -0400

    Added take a survey sidebar

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,6 +1,7 @@
 get '/users/:id' do
   if session[:user_id] == params[:id].to_i # might cause error because of string vs integer type
     @user = User.find(params[:id])
+    @all_surveys = Survey.all
     erb :'users/show'
   else
     redirect '/'
